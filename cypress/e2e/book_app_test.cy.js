c9208541cbd692f2ce09bee3d6cb03bb552dee54
migbaranes <migbaranes@gmail.com>
2024-07-11 13:03:03 +0300

    First commit

---
+++ cypress/e2e/book_app_test.cy.js
@@ -0,0 +1,61 @@
+describe("Tests BooksApp", () => {
+    beforeEach(() => {
+      cy.visit('/');
+    })
+  
+    it("Success login", () => {
+      cy.login("bropet@mail.ru", 123);
+      cy.contains("Добро пожаловать bropet@mail.ru").should("be.visible");
+    })
+  
+    it("Login with empty email", () => {
+      cy.login("", 123);
+      cy.CheckInputValidity("#mail");
+    })
+  
+    it("Login with empty password", () => {
+      cy.login("bropet@mail.ru", "");
+      cy.CheckInputValidity("#pass");
+    })
+  
+    it("Login with empty email and password", () => {
+      cy.login("", "");
+      cy.CheckInputValidity("#mail");
+      cy.CheckInputValidity("#pass");
+    })
+  
+    it("Add book", () => {
+      const title = "Title" + Date.now();
+      const author = "Author" + Date.now();
+  
+      cy.login("bropet@mail.ru", 123);
+      cy.addBook(title, author);
+      cy.get('.card-body > .card-title').last().should("have.text", title);
+      cy.get('.card-body > .card-text').last().should("have.text", author);
+    })
+  
+    it("Add book to favorites", () => {
+      const title = "Title" + Date.now();
+      const author = "Author" + Date.now();
+  
+      cy.login("bropet@mail.ru", 123);
+      cy.addBook(title, author);
+      cy.contains('.card-title', title).parent().parent().contains('button', 'Add to favorite').click();
+      cy.contains('.card-title', title).parent().parent().contains('button', 'Delete from favorite').should('be.visible');
+      cy.get('[href="/favorites"]').click();
+      cy.contains('.card-title', title).should('be.visible');
+    })
+  
+    it("Delete book from favorites", () => {
+      const title = "Title" + Date.now();
+      const author = "Author" + Date.now();
+  
+      cy.login("bropet@mail.ru", 123);
+      cy.addBook(title, author);
+      cy.contains('.card-title', title).parent().parent().contains('button', 'Add to favorite').click();
+      cy.contains('.card-title', title).parent().parent().contains('button', 'Delete from favorite').should('be.visible');
+  
+      cy.contains('.card-title', title).parent().parent().contains('button', 'Delete from favorite').click();
+      cy.contains('.card-title', title).parent().parent().contains('button', 'Add to favorite').should('be.visible');
+    })
+  })
---
+++ cypress/support/commands.js
@@ -0,0 +1,51 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", (email, password) => { 
+  cy.get(".ml-auto > .ml-2").click();
+  
+  if (email) 
+      cy.get("#mail").type(email);
+
+  if (password) 
+      cy.get("#pass").type(password);
+
+  cy.contains("Submit").click();
+})
+
+Cypress.Commands.add("CheckInputValidity", (inputName) => {
+  cy.get(inputName).then((elements) => {
+      expect(elements[0].checkValidity()).to.be.false;
+      expect(elements[0].validationMessage).to.be.eql("Заполните это поле.");
+    })
+})
+
+Cypress.Commands.add("addBook", (title, author) => {
+  cy.get('.p-0 > .btn').click();
+  cy.get('#title').type(title);
+  cy.get('#authors').type(author);
+  cy.get('form > .ml-2').click();
+})
